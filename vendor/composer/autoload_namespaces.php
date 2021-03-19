fc3607787ce2217e3a471760eae3af1832356aac
username <example@example.com>
2021-03-19 06:16:03 +0100

    Fixed Twig_Error_Syntax

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,6 +6,5 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
-    'Twig_' => array($vendorDir . '/twig/twig/lib'),
     'Mailgun' => array($vendorDir . '/mailgun/mailgun-php/src'),
 );
